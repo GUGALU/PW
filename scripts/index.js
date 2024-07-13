d615a8858a3984e6f0822caf353b23b8aaa02a76
Gustavo Polli <gugalupolli@gmail.com>
2024-07-13 14:09:50 -0300

    Começo da adição de modo claro e escuro, adição da função para copiar para o clipboard os contatos

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,37 @@
-// Selecione o botão no DOM
+// função para ativar e desativar dark mode
 function darkModeButton() {
-    const button = document.createElement('button');
-    button.id = 'dark-mode-button';
-    button.innerText = 'Modo Escuro';
+    const button = document.createElement("button");
+    button.id = "dark";
+    button.innerText = "Modo Escuro";
     document.body.appendChild(button);
 }
+function lightMode(){
+    const button = document.createElement("button");
+    button.id = "light";
+    button.innerText = "Modo Claro";
+    document.body.appendChild(button);
+}
+
+// função para copiar contatos para o clipboard/area de transferência
+document.addEventListener("DOMContentLoaded", (event) => {
+    function copyToClipboard(text) {
+        const tempInput = document.createElement("input");
+        tempInput.value = text;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+    }
+
+    const contactLinks = document.querySelectorAll(".div-contatos a");
+
+    contactLinks.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();  
+            const href = link.getAttribute("href");
+            copyToClipboard(href);
+
+            window.alert(`${href} copiado para o clipboard.`);
+        });
+    });
+});
